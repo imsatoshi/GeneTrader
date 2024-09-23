88dc103be04b35e198ea13899476c4125815d470
张佳伟(zhangjiawei01) <zhangjiawei01@inspur.com>
2024-09-23 20:33:07 +0800

    remove

--- genetic_algorithm/operators.py
+++ genetic_algorithm/operators.py
@@ -1,8 +1,8 @@
 import random
-from typing import List
+from typing import List, Tuple  # Add Tuple to the imports
 from genetic_algorithm.individual import Individual
 
-def crossover(parent1: Individual, parent2: Individual) -> tuple[Individual, Individual]:
+def crossover(parent1: Individual, parent2: Individual) -> Tuple[Individual, Individual]:  # Use Tuple from typing
     point = random.randint(1, len(parent1.genes) - 1)
     child1_genes = parent1.genes[:point] + parent2.genes[point:]
     child2_genes = parent2.genes[:point] + parent1.genes[point:]
