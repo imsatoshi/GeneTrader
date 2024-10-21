261c1118b1f941b9ba00cad8964a36df06b0cecd
张佳伟(zhangjiawei01) <zhangjiawei01@inspur.com>
2024-10-22 06:53:32 +0800

    update summary

--- backtesting_commands.sh
+++ backtesting_commands.sh
@@ -1,42 +1,8 @@
 #!/bin/bash
 
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen1_1729489793_8197 -c user_data/temp_config_1729489793_8197.json --timerange 20240401- --timeframe-detail 1m > generation_1.txt
+/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen1_1729535428_5493 -c user_data/temp_config_1729541335_5493.json --timerange 20240401- --timeframe-detail 1m > generation_1.txt
 
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen2_1729489952_1439 -c user_data/temp_config_1729489952_1439.json --timerange 20240401- --timeframe-detail 1m > generation_2.txt
+/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen2_1729542480_8503 -c user_data/temp_config_1729542480_8503.json --timerange 20240401- --timeframe-detail 1m > generation_2.txt
 
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen3_1729490009_4737 -c user_data/temp_config_1729490009_4737.json --timerange 20240401- --timeframe-detail 1m > generation_3.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen4_1729490112_3632 -c user_data/temp_config_1729490112_3632.json --timerange 20240401- --timeframe-detail 1m > generation_4.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen5_1729490284_5784 -c user_data/temp_config_1729490284_5784.json --timerange 20240401- --timeframe-detail 1m > generation_5.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen6_1729490446_1924 -c user_data/temp_config_1729490446_1924.json --timerange 20240401- --timeframe-detail 1m > generation_6.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen7_1729490545_9113 -c user_data/temp_config_1729490545_9113.json --timerange 20240401- --timeframe-detail 1m > generation_7.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen8_1729490992_8083 -c user_data/temp_config_1729490992_8083.json --timerange 20240401- --timeframe-detail 1m > generation_8.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen9_1729491090_6216 -c user_data/temp_config_1729491090_6216.json --timerange 20240401- --timeframe-detail 1m > generation_9.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen10_1729491271_1982 -c user_data/temp_config_1729491271_1982.json --timerange 20240401- --timeframe-detail 1m > generation_10.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen11_1729491369_5745 -c user_data/temp_config_1729491369_5745.json --timerange 20240401- --timeframe-detail 1m > generation_11.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen12_1729491539_3502 -c user_data/temp_config_1729491539_3502.json --timerange 20240401- --timeframe-detail 1m > generation_12.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen13_1729491663_2560 -c user_data/temp_config_1729491663_2560.json --timerange 20240401- --timeframe-detail 1m > generation_13.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen14_1729491872_5826 -c user_data/temp_config_1729491872_5826.json --timerange 20240401- --timeframe-detail 1m > generation_14.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen15_1729492004_9419 -c user_data/temp_config_1729492004_9419.json --timerange 20240401- --timeframe-detail 1m > generation_15.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen16_1729492145_6860 -c user_data/temp_config_1729492145_6860.json --timerange 20240401- --timeframe-detail 1m > generation_16.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen17_1729492441_5355 -c user_data/temp_config_1729492441_5355.json --timerange 20240401- --timeframe-detail 1m > generation_17.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen18_1729492588_1685 -c user_data/temp_config_1729492588_1685.json --timerange 20240401- --timeframe-detail 1m > generation_18.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen19_1729492645_2977 -c user_data/temp_config_1729492645_2977.json --timerange 20240401- --timeframe-detail 1m > generation_19.txt
-
-/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen20_1729492730_4647 -c user_data/temp_config_1729492730_4647.json --timerange 20240401- --timeframe-detail 1m > generation_20.txt
+/Users/zhangjiawei/Projects/freqtrade/.venv/bin/freqtrade backtesting --strategy GeneTrader_gen3_1729545074_1277 -c user_data/temp_config_1729545074_1277.json --timerange 20240401- --timeframe-detail 1m > generation_3.txt
 
